5a17098d01e203c51741c9e9a089e043b179c2d0
codershangfeng <codershangfeng@gmail.com>
2020-02-16 12:39:57 +0800

    Play metronome using play button.

--- app/src/main/java/me/codershangfeng/guitarlian/ui/metronome/MetronomeFragment.kt
+++ app/src/main/java/me/codershangfeng/guitarlian/ui/metronome/MetronomeFragment.kt
@@ -1,11 +1,12 @@
 package me.codershangfeng.guitarlian.ui.metronome
 
+import android.media.AudioManager
+import android.media.ToneGenerator
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageButton
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -15,10 +16,14 @@ import me.codershangfeng.guitarlian.R
 import me.codershangfeng.guitarlian.ui.metronome.MetronomeViewModel.Companion.DEFAULT_BEATS_PER_MINUTES
 import me.codershangfeng.guitarlian.ui.metronome.MetronomeViewModel.Companion.MAX_BEATS_PER_MINUTES
 import me.codershangfeng.guitarlian.ui.metronome.MetronomeViewModel.Companion.MIN_BEATS_PER_MINUTES
+import me.codershangfeng.guitarlian.ui.metronome.MetronomeViewModel.Companion.ONE_MINUTE_IN_MILLISECOND
+import java.util.*
 
 class MetronomeFragment : Fragment() {
 
     private lateinit var metronomeViewModel: MetronomeViewModel
+
+    private lateinit var timer: Timer
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,19 +33,55 @@ class MetronomeFragment : Fragment() {
 
         metronomeViewModel =
             ViewModelProvider(this).get(MetronomeViewModel::class.java)
+        timer = Timer()
 
         val root = inflater.inflate(R.layout.fragment_metronome, container, false)
 
+        bindingText(root)
+
+        bindingBtn(root)
+
+        bindingBar(root)
+
+        return root
+    }
+
+    private fun bindingText(root: View) {
         val textView: TextView = root.findViewById(R.id.text_beats_per_minutes)
+        metronomeViewModel.beatsPerMinute.observe(this, Observer {
+            textView.text = it
+        })
+    }
 
-        val startBtn: Button = root.findViewById(R.id.start_btn)
-        startBtn.setOnClickListener { Log.i(this::class.java.simpleName, "clicked") }
+    private fun bindingBtn(root: View) {
+        val startBtn: ImageButton = root.findViewById(R.id.start_btn)
+        val generator = ToneGenerator(AudioManager.STREAM_RING, 100)
 
+
+        startBtn.setOnClickListener {
+            timer.cancel()
+            timer = Timer()
+            val timerTask = object : TimerTask() {
+                override fun run() {
+                    generator.startTone(ToneGenerator.TONE_PROP_BEEP)
+                }
+            }
+            // TODO: Extract these business logic to ViewModel layer later
+            val bpm = metronomeViewModel.beatsPerMinute.value?.toInt() ?: DEFAULT_BEATS_PER_MINUTES
+            timer.scheduleAtFixedRate(
+                timerTask, 0, ONE_MINUTE_IN_MILLISECOND / bpm
+            )
+        }
+    }
+
+    private fun bindingBar(root: View) {
         val skbBpm: SeekBar = root.findViewById(R.id.skb_beats_per_minutes)
+        // TODO: Extract these business logic to ViewModel layer later
         skbBpm.max = MAX_BEATS_PER_MINUTES
-        skbBpm.progress = DEFAULT_BEATS_PER_MINUTES
+        skbBpm.progress = DEFAULT_BEATS_PER_MINUTES - MIN_BEATS_PER_MINUTES
         skbBpm.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                timer.cancel()
                 metronomeViewModel.beatsPerMinute.value =
                     (progress + MIN_BEATS_PER_MINUTES).toString()
             }
@@ -53,10 +94,6 @@ class MetronomeFragment : Fragment() {
                 // empty body
             }
         })
-
-        metronomeViewModel.beatsPerMinute.observe(this, Observer {
-            textView.text = it
-        })
-        return root
     }
+
 }
--- app/src/main/java/me/codershangfeng/guitarlian/ui/metronome/MetronomeViewModel.kt
+++ app/src/main/java/me/codershangfeng/guitarlian/ui/metronome/MetronomeViewModel.kt
@@ -9,8 +9,10 @@ class MetronomeViewModel : ViewModel() {
         const val MAX_BEATS_PER_MINUTES = 178
         const val DEFAULT_BEATS_PER_MINUTES = 100
         const val MIN_BEATS_PER_MINUTES = 40
+        const val ONE_MINUTE_IN_MILLISECOND: Long = 60_000
     }
 
     private val _beatsPerMinute = MutableLiveData<String>(DEFAULT_BEATS_PER_MINUTES.toString())
     val beatsPerMinute: MutableLiveData<String> = _beatsPerMinute
 }
+
