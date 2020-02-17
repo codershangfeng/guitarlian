f323492072255a9b2b248ac5678189d6f2e15ccf
codershangfeng <codershangfeng@gmail.com>
2020-02-17 21:18:17 +0800

    Use code to generate view.

--- app/src/main/java/me/codershangfeng/guitarlian/ui/tune/TuneFragment.kt
+++ app/src/main/java/me/codershangfeng/guitarlian/ui/tune/TuneFragment.kt
@@ -1,11 +1,17 @@
 package me.codershangfeng.guitarlian.ui.tune
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
@@ -13,16 +19,17 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import me.codershangfeng.guitarlian.R
 
-
+private const val LOG_TAG = "TuneFragment"
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
 
 class TuneFragment : Fragment() {
-
     private lateinit var tuneViewModel: TuneViewModel
 
     private var permissionToRecordAccepted = false
     private val permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
 
+    private var playButton: PlayButton? = null
+    private var frequencyTextView: FrequencyTextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,11 +44,29 @@ class TuneFragment : Fragment() {
             permissions,
             REQUEST_RECORD_AUDIO_PERMISSION
         )
-        val root = inflater.inflate(R.layout.fragment_tune, container, false)
 
-        bindingText(root)
+        playButton = PlayButton(context)
+        frequencyTextView = FrequencyTextView(context)
 
-        return root
+        return LinearLayout(requireContext()).apply {
+            gravity = Gravity.CENTER
+            orientation = LinearLayout.VERTICAL
+
+            addView(
+                frequencyTextView, LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    0f
+                )
+            )
+            addView(
+                playButton, LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    0f
+                )
+            )
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -56,10 +81,23 @@ class TuneFragment : Fragment() {
         if (!permissionToRecordAccepted) requireActivity().finish()
     }
 
-    private fun bindingText(root: View) {
-        val textView: TextView = root.findViewById(R.id.text_home)
-        tuneViewModel.frequency.observe(this, Observer {
-            textView.text = it.toString()
-        })
+    internal inner class PlayButton(ctx: Context? = requireContext()) : Button(ctx) {
+
+        private val clickListener = OnClickListener {
+            Log.i(LOG_TAG, "Clicked")
+        }
+
+        init {
+            text = resources.getText(R.string.text_play)
+            setOnClickListener(clickListener)
+        }
+    }
+
+    internal inner class FrequencyTextView(ctx: Context? = requireContext()) : TextView(ctx) {
+        init {
+            tuneViewModel.frequency.observe(this@TuneFragment, Observer {
+                text = it.toString()
+            })
+        }
     }
 }
